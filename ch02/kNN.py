8ecfcedbdab5bb0ee7de1e6ceed8832a2e8fbe58
liugongfeng <liugong.feng@aliyun.com>
2018-04-02 18:36:53 +0800

    Update kNN.py

--- ch02/kNN.py
+++ ch02/kNN.py
@@ -1,29 +1,54 @@
 import numpy as np
 import operator
 
+"""Constructor
+>>> group,labels = kNN.createDataSet()
+>>> group
+array([[ 1. , 1.1],
+[ 1. , 1. ],
+[ 0. , 0. ],
+[ 0. , 0.1]])
+>>> labels
+['A', 'A', 'B', 'B']
+"""
 def createDataSet():
 	group = np.array([ [1.0, 1.1], [1.0, 1.0], [0,0], [0, 0.1] ])
 
 	labels = ['A', 'A', 'B', 'B']
 	return group, labels
 
-
+"""Implement K-nearest neighbors"""
 def classify0(inX, dataSet, labels, k):
 	dataSetSize = dataSet.shape[0]		# group.shape is (4, 2)
+	# Calculate the distance:
 	diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
 	sqDiffMat = diffMat ** 2
 	sqDistances = sqDiffMat.sum(axis = 1)
 	distances = sqDistances ** 0.5
 	sortedDistIndicies = distances.argsort()
-
+	# to find k-nearest points:
 	classCount = {}
 	for i in range(k):
 		voteIlabel = labels[sortedDistIndicies[i]]
 		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+	#Sort
 	sortedClassCount = sorted(classCount.items(), key=lambda x: x[1], reverse=True)
 	return sortedClassCount[0][0]
 
+"""
+>>> datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+>>> datingDataMat	# the result is not the only one
+array([[ 7.29170000e+04, 7.10627300e+00, 2.23600000e-01],
+[ 1.42830000e+04, 2.44186700e+00, 1.90838000e-01],
+[ 7.34750000e+04, 8.31018900e+00, 8.52795000e-01],
+...,
+[ 1.24290000e+04, 4.43233100e+00, 9.24649000e-01],
+[ 2.52880000e+04, 1.31899030e+01, 1.05013800e+00],
+[ 4.91800000e+03, 3.01112400e+00, 1.90663000e-01]])
 
+>>> datingLabels[0:20]
+[3, 2, 1, 1, 1, 1, 3, 3, 1, 3, 1, 1, 2, 1, 1, 1, 1, 1, 2, 3]
+"""
 def file2matrix(filename):
 	fr = open(filename)
 	arrayOfLines = fr.readlines()
@@ -40,6 +65,8 @@ def file2matrix(filename):
 	return returnMat, classLabelVector
 
 
+"""Data-normalizing :
+newValue = (oldValue - minVals) / (maxVals - minVals)"""
 def autoNorm(dataSet):
 	minVals = dataSet.min(0)
 	maxVals = dataSet.max(0)
@@ -70,7 +97,14 @@ def datingClassTest():
 	print("the total error rate is: %f" %(errorCount / float(numTestVecs)))
 
 
-
+	
+"""
+>>> kNN.classifyPerson ()
+Percentage of time spent playing video games?10
+frequent flier miles earned per year?10000
+liters of ice cream consumed per year?0.5
+You will probably like this person:  in small does
+"""
 def classifyPerson():
 	resultList = ['not at all', 'in small does', 'in large doses']
 	percentTats = float(input("Percentage of time spent playing video games?"))
